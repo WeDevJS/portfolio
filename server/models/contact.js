00f0b55f5d721995654ec92eb1390163d6350177
Yording <yording9@gmail.com>
2016-09-30 09:57:49 -0500

    cambios

--- server/models/contact.js
+++ server/models/contact.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-mongoose.connect("mongodb://localhost/portfolio-wedevjs");
+mongoose.connect("mongodb://garra:garra302@ds041486.mlab.com:41486/events-garraxxi");
 
 var email_match=[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Format email is not validate."];
 
